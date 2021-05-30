70dccbc8e2f74bf6f25af3398195db5ccbf55120
Gentrit Abazi <gentritabazi01@gmail.com>
2021-05-30 23:10:25 +0200

    Create config file.

---
+++ src/Config/laradeploy.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    // Here you can specify from which branch you want to get changes.
+    'branch' => 'master',
+
+    // Here you can configure "secret" key to make sure the request comes from GitHub.
+    'deploy_secret' => 'RANDOM_STRING',
+
+    // Here you can enable/disable log after deploy.
+    'log_channel' => null
+
+];
