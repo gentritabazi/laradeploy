5e79bb91c7f94f2640a825c0e27a414a877daec2
Gentrit Abazi <gentritabazi01@gmail.com>
2021-05-30 23:10:32 +0200

    Create LaradeployServiceProvider.

---
+++ src/Providers/LaradeployServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GentritAbazi\Laradeploy\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaradeployServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__. '../../Config/Laradeploy.php',
+            'laradeploy'
+        );
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__. '/../Config/laradeploy.php' => config_path('laradeploy.php'),
+        ]);
+    }
+}
